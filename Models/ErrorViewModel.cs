2ea80508a5f7e5a48f2b584ae409a51bae667613
SAURAV <SAURAV@gmail.com>
2022-09-26 17:14:30 +0530

    this is my new mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
